15c6b729923ce31d01a31a6ca3affbb816604b9a
Morten Erfurt Hansen <201608514@post.au.dk>
2020-12-04 13:01:39 +0100

    Added new config with learning rate using 24 epochs

---
+++ configs/cctv/cascade_r-cnn_caviar_config_2x.py
@@ -0,0 +1,123 @@
+# The new config inherits a base config to highlight the necessary modification
+_base_ = '../cascade_rcnn/cascade_rcnn_r50_fpn_1x_coco.py'
+
+# We also need to change the num_classes in head to match the dataset's annotation
+
+# We only want to find people (person class)
+number_of_classes = 1
+
+model = dict(
+    roi_head=dict(
+        bbox_head=[
+            dict(
+                type='Shared2FCBBoxHead',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=number_of_classes,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0., 0., 0., 0.],
+                    target_stds=[0.1, 0.1, 0.2, 0.2]),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
+                               loss_weight=1.0)),
+            dict(
+                type='Shared2FCBBoxHead',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=number_of_classes,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0., 0., 0., 0.],
+                    target_stds=[0.05, 0.05, 0.1, 0.1]),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
+                               loss_weight=1.0)),
+            dict(
+                type='Shared2FCBBoxHead',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=number_of_classes,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0., 0., 0., 0.],
+                    target_stds=[0.033, 0.033, 0.067, 0.067]),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0))
+        ]))
+
+# Modify dataset related settings
+dataset_type = 'CocoDataset'
+classes = ('person',)
+
+# Similar to COCO data setup
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1333, 800),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+data = dict(
+    train=dict(
+        type=dataset_type,
+        img_prefix='configs/cctv/train/',
+        classes=classes,
+        ann_file='configs/cctv/train/annotation_coco.json',
+        pipeline=train_pipeline),
+    test=dict(
+        type=dataset_type,
+        img_prefix='configs/cctv/test/',
+        classes=classes,
+        ann_file='configs/cctv/test/annotation_coco.json',
+        pipeline=test_pipeline),
+    val=dict(
+        type=dataset_type,
+        img_prefix='configs/cctv/val/',
+        classes=classes,
+        ann_file='configs/cctv/val/annotation_coco.json',
+        pipeline=test_pipeline))
+
+evaluation = dict(interval=1, metric='bbox')
+
+lr_config = dict(step=[16, 22])
+total_epochs = 24
+
+# We can use the pre-trained Cascade RCNN model to obtain higher performance
+load_from = 'pretrained_models/cascade_rcnn_r50_fpn_1x_coco_20200316-3dc56deb.pth'
